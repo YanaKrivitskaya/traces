a1d807110f6a5e289abc2b7bdc9bf6f2f685f66c
yana <yani.krivitskaya@gmail.com>
2020-04-09 19:25:00 +0300

    main screen layout updated

---
+++ traces/lib/colorsPalette.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorsPalette {
+  static const Color orange = Colors.orange;
+  static const Color orangeBright = const Color(0xFFFFAB00);
+  static const Color greenLight = const Color(0xFF9AAC1B);
+  static const Color greenDark = const Color(0xFF3C9C50);
+  static const Color greenBlue = const Color(0xFF008371);
+  static const Color blueLight = const Color(0xFF006673);
+  static const Color blueDark = const Color(0xFF2F4858);
+
+  static const Color pinkLight = const Color(0xFFFFECCC);
+  static const Color beigeDark = const Color(0xFFA48155);
+  static const Color greenBlueDark = const Color(0xFF005248);
+
+  static const Color redBrown = const Color(0xFF591E00);
+  static const Color blueGreen = const Color(0xFF00A58C);
+  static const Color grayLight = const Color(0xFFDFE0DF);
+
+  static const Color pink = const Color(0xFFF15152);
+  static const Color dark = const Color(0xFF3A2E39);
+  static const Color light = const Color(0xFFF4D8CD);
+  static const Color blue = const Color(0xFF1E555C);
+
+  static const Color paleBlue = const Color(0xFF59BAB7); // for icon
+  static const Color back = const Color(0xFFFEFCFB); // for back
+  static const Color bordo = const Color(0xFFD3518A);
+
+  //static const Color iconTitle = const Color(0xFF47817E); //++
+  //static const Color mainColor = const Color(0xFFF15152); // ++
+  static const Color iconColor = beniukonBronze;
+  static const Color iconTitle = blueHorizon;
+  static const Color mainColor = blueHorizon;
+  static const Color backColor = back;
+  //static const Color backColor = const Color(0xFFFEFCFB); // ++
+  //static const Color backColor = const Color(0xFFFFFFEA);
+
+
+///de palette
+  static const Color fusionRed = const Color(0xFFFC5C65);
+  static const Color desire = const Color(0xFFEB3B5A);
+  static const Color highBlue = const Color(0xFF45AAF2);
+  static const Color boyzone = const Color(0xFF2D98DA);
+  static const Color orangeHibiscus = const Color(0xFFFD9644);
+  static const Color beniukonBronze = const Color(0xFFFA8231);
+  static const Color c64Ntsc = const Color(0xFF4B7BEC);
+  static const Color royalBlue = const Color(0xFF3867D6);
+  static const Color flirtatious = const Color(0xFFFED330);
+  static const Color nycTaxi = const Color(0xFFF7B731);
+  static const Color lighterPurple = const Color(0xFFA55EEA);
+  static const Color gloomyPurple = const Color(0xFF8854D0);
+  static const Color reptileGreen = const Color(0xFF26DE81);
+  static const Color algalFuel = const Color(0xFF20BF6B);
+  static const Color maxBlueGreen = const Color(0xFF2BCBBA);
+  static const Color turquoiseTopaz = const Color(0xFF0FB9B1);
+  static const Color blueGrey = const Color(0xFF778CA3);
+  static const Color blueHorizon = const Color(0xFF4B6584); //good for title and main color
+
+  //nl palette
+  static const Color sunflower = const Color(0xFFFFC312);
+  static const Color radiantYellow = const Color(0xFFF79F1F);
+  static const Color redPigment = const Color(0xFFEA2027);
+  static const Color energos = const Color(0xFFC4E538);
+  static const Color androidGreen = const Color(0xFFA3CB38);
+  static const Color turkishAqua = const Color(0xFF006266);
+  static const Color blueMartina = const Color(0xFF12CBC4);
+  static const Color meditSea = const Color(0xFF1289A7);
+  static const Color merchantMarBlue = const Color(0xFF0652DD);
+  static const Color underSea = const Color(0xFF1B1464);
+  static const Color lavenderRose = const Color(0xFFFDA7DF);
+  static const Color lavenderTea = const Color(0xFFD980FA);
+  static const Color circumorbitalRing = const Color(0xFF5758BB);
+  static const Color baraRed = const Color(0xFFED4C67);
+  static const Color veryBerry = const Color(0xFFB53471);
+  static const Color hollyhock = const Color(0xFF833471);
+  static const Color magentaPurple = const Color(0xFF4B6584);
+
+}
