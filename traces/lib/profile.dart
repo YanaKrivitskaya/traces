5b06649f4915be7bf41b2f35ab06a2a287b5e1e1
yana <yani.krivitskaya@gmail.com>
2020-04-10 20:06:29 +0300

    child views update

---
+++ traces/lib/profile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'colorsPalette.dart';
+
+class ProfilePage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _ProfilePageState();
+  }
+}
+
+class _ProfilePageState extends State<ProfilePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: GoogleFonts.quicksand(textStyle: TextStyle(color: ColorsPalette.grayLight, fontSize: 40.0))
+        ),
+        centerTitle: true
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
